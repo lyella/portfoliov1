e3cad539afb122e6cde17b8604f24dd337ae902e
lyella <ambrosioshaira@gmail.com>
2020-08-03 00:31:14 +0800

    Add files via upload

---
+++ lyellaambrosio.com/script.js
@@ -0,0 +1,217 @@
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+	
+	//Navigation
+
+	var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
+
+	
+	//Switch light/dark
+	
+	$("#switch").on('click', function () {
+		if ($("body").hasClass("light")) {
+			$("body").removeClass("light");
+			$("#switch").removeClass("switched");
+		}
+		else {
+			$("body").addClass("light");
+			$("#switch").addClass("switched");
+		}
+	});
+	
+})(jQuery);
+
+// typing effect
+
+var TxtRotate = function(el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 100) || 4000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
+  
+	if (this.isDeleting) {
+	  this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+	  this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
+  
+	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+	var that = this;
+	var delta = 150 - Math.random() * 100;
+  
+	if (this.isDeleting) { delta /= 2; }
+  
+	if (!this.isDeleting && this.txt === fullTxt) {
+	  delta = this.period;
+	  this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+	  this.isDeleting = false;
+	  this.loopNum++;
+	  delta = 1000;
+	}
+  
+	setTimeout(function() {
+	  that.tick();
+	}, delta);
+  };
+  
+  window.onload = function() {
+	var elements = document.getElementsByClassName('txt-rotate');
+	for (var i=0; i<elements.length; i++) {
+	  var toRotate = elements[i].getAttribute('data-rotate');
+	  var period = elements[i].getAttribute('data-period');
+	  if (toRotate) {
+		new TxtRotate(elements[i], JSON.parse(toRotate), period);
+	  }
+	}
+	// INJECT CSS
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #F26921 }";
+	document.body.appendChild(css);
+  };
+  
+
+
+
+//   footer
+function generateBalls() {
+	for (var i = 0; i < Math.floor(window.innerWidth/20); i++) {
+	  $(".gooey-animations").append(`
+	  <div class="ball"></div>
+	`);
+	  var colors = ['#28323B','#FFA036'];
+	  $(".ball").eq(i).css({"bottom":"0px","left":Math.random()*window.innerWidth-100,"animation-delay":Math.random()*5+"s","transform":"translateY("+Math.random()*10+"px)","background-color":colors[i%2]});
+	}
+  }
+  generateBalls();
+  
+  window.addEventListener('resize', function(e) {
+	$(".gooey-animations .ball").remove();
+	generateBalls();
+  })
+
+//   skills
+
+;( function( $ ) {
+	"use strict";
+	
+	var $bars = $( ".bar" ),
+		methods = {
+			init: function() {
+				
+				// Bind events
+				methods.bindEvents();
+				
+			},
+			bindEvents: function() {
+
+				// Loop through each of the bars...
+				$bars.each( function() {
+
+					var $bar = $( this ),
+						$pct = $bar.find( ".pct" ),
+						data = $bar.data( "bar" );
+
+					setTimeout( function() {
+
+						$bar
+							.css( "background-color", data.color )
+							.animate({
+								"width": $pct.html()
+							}, data.speed || 3000, function() {
+
+								$pct.css({
+									"color": data.color,
+									"opacity": 1
+								});
+
+							});
+
+					}, data.delay || 0 );			
+
+				});
+
+			}
+		};
+	
+	// Initialize on page load
+	methods.init();	
+		
+})( jQuery );
+
+// this is a separated JS for my works. 
+$(function() {
+
+    $('.toggles button').click(function(){
+      var get_id = this.id;
+      var get_current = $('.posts .' + get_id);
+  
+        $('.post').not( get_current ).hide(500);
+        get_current.show(500);
+    });
+    
+    
+    $('#showall').click(function() {
+        $('.post').show(500);
+    });
+
+
+}); 
